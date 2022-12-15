12d376b34d27482e3d3e9ef52808ab3b5f807f52
Nitish Kafle <kaflenit@gmail.com>
2022-12-14 18:16:01 -0600

    starter files - updated form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,38 @@
+import { useState } from "react";
+
 export const Form = () => {
+  const [fullName, setFullName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <form>
-      <input type="text" placeholder="Name" name="fullname" />
-      <input type="text" placeholder="username" name="username" />
-      <input type="text" placeholder="email" name="email" />
-      <input type="password" placeholder="password" name="password" />
+      <input
+        type="text"
+        placeholder="Name"
+        name="fullname"
+        onChange={(e) => setFullName(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="username"
+        name="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="email"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
     </form>
   );
 };
